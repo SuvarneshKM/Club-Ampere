95de102cea5784cf34910c957020a7df053a799b
AkshayBenny <akshaybennygh@gmail.com>
2022-01-17 00:44:06 +0530

    header + hero

---
+++ components/hero/index.js
@@ -0,0 +1,26 @@
+function Hero() {
+  return (
+    <div
+      className='pt-6 text-center bg-contain bg-center bg-no-repeat'
+      style={{ backgroundImage: `url(./images/banner.png)` }}
+    >
+      <h2 className='pt-36 uppercase font-Jost font-bold md:text-5xl text-xl text-black text-opacity-60'>
+        Club
+      </h2>
+      <h1 className='pb-2 uppercase font-Jost font-bold md:text-9xl text-2xl text-black'>
+        AMPERE
+      </h1>
+      <p className='font-normal md:text-lg text-sm text-black'>
+        Aliquam sit massa feugiat sapien hac purus. Dictum in habitant eu vitae
+        dolor.
+      </p>
+      <div className='pt-16 pb-24'>
+        <button className='bg-secondary px-6 py-3 rounded-md text-white md:text-base text-sm font-medium'>
+          Join now
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,13 +1,32 @@
 function Header() {
-    return (
-        <div>
-            <h1>I am Header Headers</h1>
-            <h1>I am Header Headers</h1>
-            <h1>I am Header Headers</h1>
-            <h1>I am Header Headers</h1>
-            <h1>I am Header Headers</h1>
-        </div>
-    )
+  return (
+    <div className='flex justify-between text-center items-center'>
+      <img
+        className='md:w-8 w-6 cursor-pointer'
+        src='/images/logo.png'
+        alt='Club Ampere'
+      />
+      <ul className='hidden md:flex space-x-12 text-center items-center text-sm font-medium'>
+        <li className='text-black text-opacity-50 hover:text-opacity-100 cursor-pointer'>
+          Events
+        </li>
+        <li className='text-black text-opacity-50 hover:text-opacity-100 cursor-pointer'>
+          Guide
+        </li>
+        <li className='text-black text-opacity-50 hover:text-opacity-100 cursor-pointer'>
+          Team
+        </li>
+        <li className='text-black text-opacity-50 hover:text-opacity-100 cursor-pointer'>
+          Contact
+        </li>
+      </ul>
+      <div>
+        <button className='bg-secondary px-6 py-2 rounded-md text-white md:text-base text-sm font-medium'>
+          Join
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
