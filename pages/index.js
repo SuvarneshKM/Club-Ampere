e22db05ebf13dc49bf9594205fe0e08b569f7dd2
AkshayBenny <akshaybennygh@gmail.com>
2022-01-17 00:44:06 +0530

    header + hero

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,28 @@
-import Head from 'next/head'
-import Layout from '../components/layout'
+import Head from 'next/head';
+import Hero from '../components/hero';
+import Layout from '../components/layout';
 
 export default function Home() {
   return (
-    <div className='font-poppins'>
-      <Head>
-        <title>Club Ampere ⚡</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" />      </Head>
-      <Layout>
-        <h1 className=''>Club Ampere ⚡</h1>
-      </Layout>
+    <div className='font-poppins bg-bg md:px-16 px-6 md:pt-6 pt-4'>
+      <div className='max-w-7xl mx-auto'>
+        <Head>
+          <title>Club Ampere ⚡</title>
+          <link rel='icon' href='/favicon.ico' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+            rel='stylesheet'
+          />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Jost:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <Layout>
+          {/* <h1 className='text-primary text-opacity-50'>Club Ampere ⚡</h1> */}
+          <Hero />
+        </Layout>
+      </div>
     </div>
-  )
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
       fontFamily: {
         poppins: "'Poppins', sans-serif",
+        Jost: "'Jost', sans-serif",
+      },
+      colors: {
+        primary: '#F28130',
+        secondary: '#FEC432',
+        light: '#FFF8E5',
+        bg: '#F6F6F6',
       },
     },
   },
   plugins: [],
-}
+};
