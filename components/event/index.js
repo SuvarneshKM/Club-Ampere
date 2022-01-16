d911d654215de2d8cc8b58e4056b419a8a42aa9c
AkshayBenny <akshaybennygh@gmail.com>
2022-01-17 01:09:21 +0530

    events

---
+++ components/event/index.js
@@ -0,0 +1,14 @@
+import Card from './card';
+
+function Event() {
+  return (
+    <div>
+      <h1 className='font-Jost text-center font-bold md:text-5xl text-2xl text-black'>
+        Event
+      </h1>
+      <Card />
+    </div>
+  );
+}
+
+export default Event;
