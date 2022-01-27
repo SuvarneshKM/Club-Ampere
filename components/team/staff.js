90a3168fe87787dd5863058df04eacadce3857d3
suvarneshkm <suvarnesh1729@gmail.com>
2022-01-27 19:51:33 +0530

    core and staff

--- components/team/staff.js
+++ components/team/staff.js
@@ -1,7 +1,7 @@
 export default function Staff({ name, image, position }) {
     return (
         <div className="text-center flex flex-col justify-center items-center py-7">
-            <img src={`https://club-ampere-backend.herokuapp.com${image}`} alt={name} className="w-24 h-24 object-cover rounded-full" />
+            <img src={image} alt={name} className="w-24 h-24 object-cover rounded-full" />
             <p className="font-semibold text-sm text-white pt-4">{name}</p>
             <p className="font-semibold text-sm text-secondary text-opacity-70">{position}</p>
         </div>
